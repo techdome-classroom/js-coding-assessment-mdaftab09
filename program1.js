57ce134e1402d1731631e52cfee277169d76ab7e
mdaftab09 <103241110+mdaftab09@users.noreply.github.com>
2024-11-11 14:02:42 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,6 +2,39 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
+function generateRandomGrid(rows, cols, landProbability = 0.5) {
+  const grid = [];
+  
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      
+      row.push(Math.random() < landProbability ? 'L' : 'W');
+    }
+    grid.push(row);
+  }
+
+  const midRow = Math.floor(rows / 2);
+  const midCol = Math.floor(cols / 2);
+  if (grid[midRow][midCol] === 'W') {
+    grid[midRow][midCol] = 'L';
+  }
+  if (grid[midRow + 1] && grid[midRow + 1][midCol] === 'W') {
+    grid[midRow + 1][midCol] = 'L';
+  }
+  if (grid[midRow][midCol + 1] === 'W') {
+    grid[midRow][midCol + 1] = 'L';
+  }
+
+  return grid;
+}
+
+
+const randomGrid = generateRandomGrid(5, 5);
+console.log(randomGrid);
+
+console.log("Number of Islands:", numIslands(randomGrid));
+
 
 };
 
